fd2cdc590cfe534aa5fb8efee7bc4d7222f46502
nikolai <niko.mak26@gmail.com>
2024-11-02 18:00:43 +0530

    fixed pricing page with plan data from Razorpay

--- features/payments/server/plans.ts
+++ features/payments/server/plans.ts
@@ -1,10 +1,60 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server"
 
-import { db } from "@/features/db/db"
+// import { db } from "@/features/db/db"
 
+
+
+// Fetch plans from Razorpay API
 export const getPlans = async () => {
-    // const plans = await db.plan.findMany();   
-    // return plans
+    let plan_ids;
+    if (process.env.PLAN_IDS) { 
+        plan_ids = process.env.PLAN_IDS.split(',');
+    }
+    const authToken = btoa(`${process.env.RAZORPAY_KEY_ID}:${process.env.RAZORPAY_KEY_SECRET}`);
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Basic ' + authToken
+        }
+    };
 
-    return db.plan.findMany();
+
+    // temporary storage to combine all plans
+    const plans: any = [];
+    
+    // iterate over all plans from the configuration
+    for (const id of plan_ids || []) {
+    try {
+        // fetch the plan details from Razorpay API using the plan ID
+        const response = await fetch(`https://api.razorpay.com/v1/plans/${id}`, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // parse the response JSON 
+        const data = await response.json();
+
+        // transform the amount to cents 
+        data.item.amount = data?.item?.amount / 100;
+
+        // transform the notes to an array of key-value pairs
+        data.item.notes = Object.entries(data?.notes).map(([key, item]) => {
+
+        // transform the key format from "notes_key_1" to "Notes key 1"
+        const newKey = key.replace(/_/g, ' ').replace(/(?:^|\s)\S/g, (a) => a.toUpperCase());
+        return [newKey, item];  
+        });
+        plans.push(data);
+       
+        // await db.collection('plans').insertOne(data);
+        
+       
+        // return combined and formatted plans
+    } catch (error: any) {
+        console.error("Cannot fetch plans, due to:" , error)
+    }
+};
+
+return plans;
 }
---
+++ features/payments/server/subscriptions.ts
@@ -0,0 +1,5 @@
+import { db } from "@/features/db/db"
+
+export const buySubscription = async () => {
+    
+}
