94e67acec012b53650e20f8138ef5b93d14ae569
nikolai <niko.mak26@gmail.com>
2024-11-05 16:51:29 +0530

    added subscription activation logic

--- features/payments/server/subscriptions.ts
+++ features/payments/server/subscriptions.ts
@@ -1,5 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server"
+
 import { db } from "@/features/db/db"
 
-export const buySubscription = async () => {
+export const activateSubscription = async (userId : any, planId : any) => {
+    // get user from db
+    const user = await db.user.findUnique({
+        where :{ 
+            id: userId,
+        }
+    });
+
+    // return if user not found
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    // check if user already has a subscription
+    const subscription = await db.subscription.findFirst({
+        where: {
+            userId: userId,
+        }
+    });
+
+    if (subscription) {
+        throw new Error("User already has a subscription");
+    }
+
+
+    const authToken = btoa(`${process.env.RAZORPAY_KEY_ID}:${process.env.RAZORPAY_KEY_SECRET}`);
+    const options:any = {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + authToken,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            plan_id: planId,
+            quantity: 1,
+            total_count: 12
+        })
+    }
+    // create new subscription
+    const newSubscription = await fetch(`https://api.razorpay.com/v1/subscriptions/`, options);
+    const data = await newSubscription.json();
     
+    if (data?.error) {
+        throw new Error(`HTTP error! status: ${data?.error?.code}`);
+    }
+
+    return data;
 }
+
+
+
+
+// // Add subscription to database.
+// export const updateSubscriptions = async(userId : any, subscription_id : any) => {
+
+
+// }
