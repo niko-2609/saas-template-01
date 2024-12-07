ec8ef234cc4d04cf21a50479db048c08e1ddd6f6
nikolai <niko.mak26@gmail.com>
2024-12-07 15:13:59 +0530

    added dashboard page and payment error page

--- middleware.ts
+++ middleware.ts
@@ -12,15 +12,7 @@ import {
     DEFAULT_PUBLIC_REDIRECT
 } from "@/routes"
 import { NextRequest, NextResponse } from "next/server"
-// import { createCsrfMiddleware } from '@edge-csrf/nextjs';
-// import { getExpectedToken } from "./features/authentication/server/verification"
 
-// // initalize csrf protection middleware
-// const csrfMiddleware = createCsrfMiddleware({
-//     cookie: {
-//         secure: process.env.NODE_ENV === 'production',
-//     },
-// });
 
 const { auth } = NextAuth(authConfig);
 
@@ -29,30 +21,30 @@ export default auth((req) => {
 
     const response = NextResponse.next();
     // // get the url and user authentication status from the request  
-    // const { nextUrl } = req
+    const { nextUrl } = req
 
 
-    // const isLoggedIn = !!req.auth
+    const isLoggedIn = !!req.auth
 
-    // // check which route is the request incoming 
-    // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    // check which route is the request incoming 
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    // if (isApiAuthRoute) {
-    //     return NextResponse.next();
-    // }
+    if (isApiAuthRoute) {
+        return NextResponse.next();
+    }
 
-    // if (isAuthRoute) {
-    //     if (isLoggedIn) {
-    //         return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    //     }
-    //     return NextResponse.next();
-    // }
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        }
+        return NextResponse.next();
+    }
 
-    // if (!isPublicRoute && !isLoggedIn) {
-    //     return NextResponse.redirect(new URL(DEFAULT_PUBLIC_REDIRECT, nextUrl))
-    // }
+    if (!isPublicRoute && !isLoggedIn) {
+        return NextResponse.redirect(new URL(DEFAULT_PUBLIC_REDIRECT, nextUrl))
+    }
     return NextResponse.next();
 })
 
